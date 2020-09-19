368ccedf9aa3541fe10a40d947376b91993383a1
md shamim <shamim.webappick@gmail.com>
2020-09-19 19:18:38 +0600

    plugin name changed

--- unlimited-theme-addons.php
+++ unlimited-theme-addons.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* Plugin Name: Codepopular Theme Helper
+* Plugin Name: Unlimited Theme Addons
 * Plugin URI: https://github.com/programmershamim/digitalshop-core
 * Description: After install the DigitalShop WordPress Theme, you must need to install this "DigitalShop Core" first to get all functions of DigitalShop WP Theme.
 * Version: 1.0.0
 * Author: codepopular
 * Author URI: https://www.codepopular.com
-* Text Domain: cpthelper
+* Text Domain: unlimited-theme-addons
 * License: GPL/GNU.
 * Domain Path: /languages
 */
@@ -18,13 +18,5 @@
 include_once(dirname( __FILE__ ). '/inc/elementor/elementor.php');
 include_once(dirname( __FILE__ ). '/inc/social-share.php');
 include_once(dirname( __FILE__ ). '/inc/widget-download-filters.php');
-
-
-
-
-
-
-
-
 
  ?>
