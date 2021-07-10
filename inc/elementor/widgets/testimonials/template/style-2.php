af2e3dc096481959eedcbd231725cbff9237d56f
bokhtyer <mdbokhtyerabid@gmail.com>
2021-07-10 17:00:02 +0600

    update testimonial

--- inc/elementor/widgets/testimonials/template/style-2.php
+++ inc/elementor/widgets/testimonials/template/style-2.php
@@ -4,10 +4,10 @@
     <div class="testimonial-item2">
         <div class="thumbnail">
             <img src="<?php echo esc_url( $testimonial['image']['url'] ); ?>" alt="<?php echo esc_attr( $testimonial['name'] ); ?>">
-            <h2 <?php echo esc_html(  $this->get_render_attribute_string( $name ) ); ?>><?php echo esc_html( $testimonial['name'] ); ?></h2>
-            <h5 <?php echo esc_html( $this->get_render_attribute_string( $designation ) ); ?>><?php echo esc_html( $testimonial['designation'] ); ?></h5>
         </div>
         <div class="content">
+            <h2 <?php echo esc_html(  $this->get_render_attribute_string( $name ) ); ?>><?php echo esc_html( $testimonial['name'] ); ?></h2>
+            <h5 <?php echo esc_html( $this->get_render_attribute_string( $designation ) ); ?>><?php echo esc_html( $testimonial['designation'] ); ?></h5>
             <?php if ( 'on' == $settings['uta-testimonial-ratings'] ) {?>
                 <div class="ratting">
                     <span><i class="fas fa-star"></i></span>
